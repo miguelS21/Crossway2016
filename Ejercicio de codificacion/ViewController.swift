a51586da64de9685966e147dad074b6f9cc15f30
Miguel Moreno <migueangmoreno@gmail.com>
2016-11-07 15:23:13 -0500

    Primera versión

--- Ejercicio de codificacion/ViewController.swift
+++ Ejercicio de codificacion/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var nameButton1: UIButton!
+    @IBOutlet weak var nameButton2: UIButton!
+    @IBOutlet weak var nameButton3: UIButton!
+    @IBOutlet weak var nameButton4: UIButton!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessage1(sender: AnyObject) {
+        messageLabel.text = "\(nameButton1.titleLabel!.text!):Miguel"
+    }
 
+    @IBAction func changeMessage2(sender: AnyObject) {
+    messageLabel.text = "\(nameButton2.titleLabel!.text!):Moreno"
+    }
+    
+    @IBAction func changeMessage3(sender: AnyObject) {
+    messageLabel.text = "\(nameButton3.titleLabel!.text!):Pasto"
+    }
+    
+    @IBAction func changeMessage4(sender: AnyObject) {
+     messageLabel.text = "\(nameButton4.titleLabel!.text!):Colombia"
+    }
+  
+    
 }
 
